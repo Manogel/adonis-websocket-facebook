0cbf27b707adf9033bf99c8af808e3ca659d0acf
Manogel <manoel_ggomes@hotmail.com>
2020-01-28 12:13:14 -0300

    notification user->post->user

--- app/Models/Hooks/LikePostHook.js
+++ app/Models/Hooks/LikePostHook.js
@@ -2,6 +2,7 @@
 
 const Ws = use('Ws')
 const Like = use('App/Models/LikePost')
+const Notification = use('App/Models/Notification')
 const LikePostHook = exports = module.exports = {}
 
 LikePostHook.method = async (modelInstance) => {
@@ -12,5 +13,17 @@ LikePostHook.sendWs = async like => {
   if (topic) {
     const [ RowDataPacket ] = await Like.query().where('post_id', like.post_id).count()
     topic.broadcast('likes', { count: RowDataPacket['count(*)'], id: like.post_id })
+  }
+}
+
+LikePostHook.notifyUser = async likePost => {
+  const post = await likePost.post().fetch()
+
+  if (post.user_id !== likePost.user_id) {
+    const user = await likePost.user().fetch()
+    await Notification.create({
+      user_id: post.user_id,
+      content: `<b>${user.username}</b> curtiu seu post`
+    })
   }
 }
